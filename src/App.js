88cf7510eec33f2860b2574a57447153c16f5496
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-17 12:18:30 +0530

    message

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Addbook from './Component/Addbook';
+import Search from './Component/Search';
+import Header from './Component/Header';
+
+function App() {
+  return (
+   <>
+    <Addbook/>
+    <Search/>
+  
+  </>
+  )
+}
+
+export default App;
---
+++ src/Component/Search.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './Header'
+
+const Search = () => {
+  return (
+      <div>
+<Header/>
+     
+    <div className="container">
+    <div className="row">
+        <div className="col col-12 col-md-12 col-sm-12 col-xxl-12 col-xl-12">
+            <div className="row">
+                <div className="col col-md-12 col-sm-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Booktitle</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                
+               
+                <div classNameName="col col-md-6 col-sm-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+  )
+}
+
+export default Search
